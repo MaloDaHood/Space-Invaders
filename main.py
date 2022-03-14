a737b660db68f858f44546a70024e77c535cc29a
MaloDaHood <malomr2002@gmail.com>
2022-03-14 18:36:47 +0100

    Clean main file

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import pygame
-import random
 
-pygame.init()
-window = pygame.display.set_mode((800, 800))
-pygame.display.set_caption("Space Invaders")
+from player import Player
+from game import Game
 
+if __name__ == "__main__":
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.flip()
+    pygame.init()
+    game = Game()
 
-pygame.quit()
+    # We start the game
+    game.run()
