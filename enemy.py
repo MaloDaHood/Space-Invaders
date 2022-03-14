8b8fa5d769e9a9f3bb884b7a928f8e26c919a33c
MaloDaHood <malomr2002@gmail.com>
2022-03-14 18:36:34 +0100

    Basic enemy class

---
+++ enemy.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+
+COLORS = ["R", "G", "B"]
+
+class Enemy:
+    
+    def __init__(self) -> None:
+        
+        # We set the enemy's starting position using a random x value
+        self.position = (random.randint(50, 750), 50)
+        
+        # We load the enemy's image using a random color
+        self.image = pygame.image.load("assets/enemy_" + random.choice(COLORS) + ".png")
+        
+    # We return the enemy's position
+    def get_position(self) -> tuple[int, int]:
+        return self.position
+    
+    # We modify the enemy's position
+    def set_position(self, new_postion :tuple[int, int]) -> None:
+        self.position = new_postion
+    
+    # We return the enemy's image
+    def get_image(self) -> pygame.surface.Surface:
+        return self.image
+    
+    # We move the enemy by one pixel towards the bottom of the screen
+    def move_down(self) -> None:
+        self.position = (self.position[0], self.position[1] + 1)
