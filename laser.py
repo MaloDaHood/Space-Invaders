30eaee105d8d67d0dff90769f01c8e484ecd47e3
MaloDaHood <malomr2002@gmail.com>
2022-03-14 19:17:09 +0100

    Basic laser class

---
+++ laser.py
@@ -0,0 +1,34 @@
+from msilib.schema import Directory
+import pygame
+
+from player import Player
+from enemy import Enemy
+
+class Laser:
+    
+    def __init__(self, object :Player|Enemy) -> None:
+        
+        if isinstance(object, Player):
+            
+            self.position = (object.get_position()[0], object.get_position()[1] - (object.get_image().get_height() // 2))
+        
+            self.image = pygame.image.load("assets/laser_player.png")
+            
+            self.direction = -6
+        
+        else:
+            
+            self.position = (object.get_position()[0] + (object.get_image().get_width() // 2), object.get_position()[1] + (object.get_image().get_height() // 2))
+        
+            self.image = pygame.image.load("assets/laser_" + object.get_color() + ".png")
+        
+            self.direction = 6
+            
+    def get_position(self) -> tuple[int, int]:
+        return self.position
+    
+    def get_image(self) -> pygame.surface.Surface:
+        return self.image
+            
+    def move(self) -> None:
+        self.position = (self.position[0], self.position[1] + self.direction)
