a9674676b01f467c6bf953e89aac376215373c54
MaloDaHood <malomr2002@gmail.com>
2022-03-17 23:19:57 +0100

    Finished basic game menu

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ if __name__ == "__main__":
     
     menu = Menu()
     
-    menu.display()
+    # We check if the player wants to play
+    if menu.display() == 1:
         
-    game = Game(menu.window)
+        # We pass the same window as the menu to the game
+        game = Game(menu.window)
 
-    # We start the game
-    game.run()
+        # We start the game
+        game.run()
     
     # We quit the game and end the program
     pygame.quit()
--- menu.py
+++ menu.py
@@ -18,38 +18,55 @@ class Menu:
         
         self.__running = True
         
-        self.__cursor = pygame.Rect(pygame.mouse.get_pos(), (10, 10))
+        # We create a rectangle on the cursor to track it
+        self.__cursor = pygame.Rect(pygame.mouse.get_pos(), (1, 1))
         
+        # We create a rectangle representing the play button
         self.__play_button = pygame.Rect((constants.WINDOW_WIDTH // 4, constants.WINDOW_HEIGHT // 4), (constants.WINDOW_WIDTH // 2, constants.WINDOW_HEIGHT // 8))
         
+        # We create a rectangle representing the quit button
         self.__quit_button = pygame.Rect((constants.WINDOW_WIDTH // 4, constants.WINDOW_HEIGHT - (constants.WINDOW_HEIGHT // 2)), (constants.WINDOW_WIDTH // 2, constants.WINDOW_HEIGHT // 8))
         
         # We load the font
-        self.__font = pygame.font.Font("assets/akira.otf", 60)
+        self.__font = pygame.font.Font("assets/akira.otf", constants.MENU_FONT_SIZE)
         
-    def display(self) -> None:
+    def display(self) -> int:
         
+        # Main loop
         while self.__running:
             
-            self.handle_inputs()
-            
+            # We display the background to the screen
             self.window.blit(self.__backgroud, (0, 0))
             
+            # We update the cursor's position
             self.__cursor.topleft = pygame.mouse.get_pos()
+                        
+            # We display the play button to the screen
+            pygame.draw.rect(self.window, constants.RED, self.__play_button, 2)
             
-            pygame.draw.rect(self.window, (255, 0, 0), self.__cursor)
+            # We display the quit button to the screen
+            pygame.draw.rect(self.window, constants.RED, self.__quit_button, 2)
             
-            pygame.draw.rect(self.window, (255, 0, 0), self.__play_button, 2)
-            
-            pygame.draw.rect(self.window, (255, 0, 0), self.__quit_button, 2)
-            
+            # We display all the text to the screen
             self.display_text()
             
+            # We get the player's input
+            input = self.handle_inputs()
+            
+            # We check if the player pressed one of the buttons
+            if input != 0:
+                
+                # We return what the player chose
+                return input
+            
+            # We update the window to show the modifications
             pygame.display.flip()
             
+        # Program shouldn't get here
+        return -1
             
-    # We handle every input given by the player
-    def handle_inputs(self) -> None:
+    # We handle every input given by the player and return 0 if no input was received, 1 if the player wants to play or -1 if he wants to exit the game
+    def handle_inputs(self) -> int:
         
         # We check each event that happened
         for event in pygame.event.get():
@@ -57,20 +74,34 @@ class Menu:
             # We check if the player pressed the close buton
             if event.type == pygame.QUIT:
                 
-                # We stop the game
-                pygame.quit()
+                return -1
             
             # We check if the player pressed the mouse 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 
-                pass
+                # We check if the player clicked the play button
+                if self.__cursor.colliderect(self.__play_button):
+                    
+                    return 1
+                
+                # We check if the player clicked the quit button
+                if self.__cursor.colliderect(self.__quit_button):
+                    
+                    return -1
+                
+        return 0
             
+    # We display all the text on the window
     def display_text(self) -> None:
         
-        text = self.__font.render("PLAY", True, (255, 0, 0))
+        # We set the text to what we need for the play button
+        text = self.__font.render("PLAY", True, constants.RED)
         
-        self.window.blit(text, (self.__play_button.x + ((constants.WINDOW_WIDTH // 4) - (text.get_width() // 2)) , self.__play_button.y + ((constants.WINDOW_HEIGHT // 8) - (text.get_height() * 1.5))))
+        # We display the text at the right coordinates
+        self.window.blit(text, (self.__play_button.x + ((constants.WINDOW_WIDTH // 4) - (text.get_width() // 2)) , self.__play_button.y + ((constants.WINDOW_HEIGHT // 8) - (text.get_height() * 1.75))))
 
-        text = self.__font.render("QUIT", True, (255, 0, 0))
+        # We set the text to what we need for the quit button
+        text = self.__font.render("QUIT", True, constants.RED)
         
-        self.window.blit(text, (self.__quit_button.x + ((constants.WINDOW_WIDTH // 4) - (text.get_width() // 2)) , self.__quit_button.y + ((constants.WINDOW_HEIGHT // 8) - (text.get_height() * 1.5))))
+        # We display the text at the right coordinates
+        self.window.blit(text, (self.__quit_button.x + ((constants.WINDOW_WIDTH // 4) - (text.get_width() // 2)) , self.__quit_button.y + ((constants.WINDOW_HEIGHT // 8) - (text.get_height() * 1.75))))
